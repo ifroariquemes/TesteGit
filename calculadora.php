ef62bc57f153eaf67042565c2109828f31f6cea1
Natanael Simoes <natanaelsimoes@gmail.com>
2017-10-31 15:10:04 -0400

    calculadora

---
+++ calculadora.php
@@ -0,0 +1,17 @@
+<?php
+
+function soma($a, $b) {
+	// retornar soma
+}
+
+function diferenca($a, $b) {
+	// retornar diferenca
+}
+
+function multiplica($a, $b) {
+	// retornar produto
+}
+
+print_r(soma(1, 2);
+print_r(diferenca(4, 3));
+print_r(multiplica(2, 2));
